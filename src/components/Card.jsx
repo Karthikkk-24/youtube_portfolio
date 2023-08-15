289d0554a9f5ca0e5a4ba6c9277b9aeda89055fb
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2023-08-15 23:54:03 +0530

    Added images and created cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,18 @@
 import "../../src/App.css";
 
-export default function Card() {
-  return <div className="card_div"></div>;
+export default function Card(props) {
+  return (
+    <div className="card_div">
+      <div className="card_img">
+        <img src={props.image} alt="Card" />
+      </div>
+      <div className="card_title">
+        <img src="../src/assets/profile.jpg" alt="" />
+        <p>{props.title}</p>
+      </div>
+      <div className="card_desc">
+        Read More ... <span>&#187;</span>
+      </div>
+    </div>
+  );
 }
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,12 +1,38 @@
 import "../../src/App.css";
-import CardBar from "../../src/components/CardBar";
+import Card from "../../src/components/Card";
 export default function MainSection() {
   return (
     <div className="container">
-      <CardBar />
-      <CardBar />
-      <CardBar />
-      <CardBar />
+      <Card
+        image="../src/assets/programming_language/js.png"
+        title="JavaScript"
+      />
+      <Card
+        image="../src/assets/programming_language/react.png"
+        title="React"
+      />
+      <Card image="../src/assets/programming_language/html.png" title="HTML" />
+      <Card image="../src/assets/programming_language/css.png" title="CSS" />
+      <Card image="../src/assets/programming_language/php.png" title="PHP" />
+      <Card image="../src/assets/programming_language/node.png" title="Node" />
+      <Card
+        image="../src/assets/programming_language/express.png"
+        title="Express"
+      />
+      <Card
+        image="../src/assets/programming_language/mysql.png"
+        title="MySQL"
+      />
+      <Card
+        image="../src/assets/programming_language/python.png"
+        title="Python"
+      />
+      <Card image="../src/assets/programming_language/java.png" title="Java" />
+      <Card image="../src/assets/programming_language/bash.png" title="Bash" />
+      <Card
+        image="../src/assets/programming_language/github.png"
+        title="GitHub"
+      />
     </div>
   );
 }
